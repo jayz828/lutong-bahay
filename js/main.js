6cfb5fefe61283dda03d3cf571240ceb7a55c3b0
Jason Natividad <jainatividad@yahoo.com>
2017-03-23 23:47:30 -0700

    Add javascript file and make menu icons disappear at lower resolution to make room for mobile menu

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/**
+ * Created by jasonnatividad on 3/23/17.
+ */
+
+
+(function Main(){
+
+
+    //var test = document.getElementsByClassName("menu-icon");
+    //
+    //console.log(test);
+    ////alert(windowWidth);
+    //
+    //
+    //window.onresize = function(){
+    //    var windowWidth = window.innerWidth;
+    //
+    //    var icons = [];
+    //
+    //
+    //    if (windowWidth < 500) {
+    //
+    //
+    //
+    //        icons = document.getElementsByClassName("menu-icon");
+    //        //icons[1].remove("fa-2x");
+    //
+    //        console.log(icons);
+    //
+    //        for (var i = 0; i < icons.length; i++) {
+    //
+    //            //console.log(i);
+    //            test[i].remove("fa-2x");
+    //        }
+    //
+    //    }
+    //}
+
+
+})();
